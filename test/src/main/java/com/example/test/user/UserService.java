a2c269baedd8bf31668d1d2e5bf05d9007ea5038
thdwjdrl401 <thdwjdrl401@naver.com>
2023-11-16 17:03:00 +0900

    사용자 이름 추가

---
+++ test/src/main/java/com/example/test/user/UserService.java
@@ -0,0 +1,34 @@
+package com.example.test.user;
+
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.Optional;
+import com.example.test.DataNotFoundException;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	public SiteUser create(String username, String name, String email, String password) {
+		SiteUser user = new SiteUser();
+		user.setUsername(username);
+		user.setEmail(email);
+		user.setName(name);
+		user.setPassword(passwordEncoder.encode(password));
+		this.userRepository.save(user);
+		return user;
+	}
+
+	public SiteUser getUser(String username) {
+		Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
+		if (siteUser.isPresent()) {
+			return siteUser.get();
+		} else {
+			throw new DataNotFoundException("siteuser not found");
+		}
+	}
+}
